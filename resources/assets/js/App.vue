1cdcdc86da85b4f1642ab0b70e561cdfa5697896
larsurilch <larsurilch@gmail.com>
2017-09-27 21:38:39 -0300

    Projeto - Parte 1 Atualização dos arquivos welcome.blade.php e app.js Inclusão do arquivo App.vue Remoção do arquivo Example.vue

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <div class="ui main container">
+            <h4>Iniciando com o Vue.js</h4>
+            <div class="ui input">
+                <input type="text" name="titulo" v-model="titulo"/>
+            </div>
+            <div>{{ titulo }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                titulo: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #app {
+        padding: 55px 0 30px;
+    }
+</style>
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,16 +9,18 @@ require('./bootstrap');
 
 require('./main');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(VueResource);
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.http.options.root = 'http://codegeo.local';
 
-const app = new Vue({
-    el: '#app'
+Vue.use(VueRouter);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
 });
